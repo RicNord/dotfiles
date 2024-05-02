64427fe16b68afe0c204be19e2d14b8c0d0eaa15
Richard Nordström <git.nordstrom@gmail.com>
2024-05-03 00:21:14 +0200

    improve partition encryption

--- scripts/encrypt-partition.sh
+++ scripts/encrypt-partition.sh
@@ -3,34 +3,24 @@
 # This scrip will encrypt an already existing partition and then creates a
 # filesystem on the partition.
 
-# TODO: verifications of correctly selected partition
-# verbose logging
-# selection of filesystem
-# sudo -A inside script
-
 # Bash stric mode
-set -eo pipefail
+set -euxo pipefail
 
 # Colors
+RED='\033[0;31m'
 BBlue='\033[1;34m'
 NC='\033[0m' # NO COLOR
-
-# Check if user is root
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root." 1>&2
-    exit 1
-fi
 
 # Get user input for the settings
 echo -e "${BBlue}The following disks are available on your system:\n${NC}"
 lsblk | grep -v 'rom' | grep -v 'loop'
 echo -e "\n"
 
-echo -e "${BBlue}WARNING ALL CURRENT DATA ON THE PARTITION WILL BE LOST.\n${NC}"
+echo -e "${RED}WARNING ALL CURRENT DATA ON THE PARTITION WILL BE LOST.\n${NC}"
 read -rp 'Select the target partition. Example sda1 or nvme0n1p1: ' TARGET_PARTITION
 echo -e "\n"
 
-cryptsetup luksFormat \
+sudo cryptsetup luksFormat \
     --verbose \
     --verify-passphrase \
     --cipher aes-xts-plain64 \
@@ -43,15 +33,21 @@ cryptsetup luksFormat \
 echo -e "\n"
 
 echo -e "${BBlue}Decrypt/open partition \n${NC}"
-cryptsetup --verbose open "/dev/$TARGET_PARTITION" tmpdecrypted
+sudo cryptsetup --verbose open "/dev/$TARGET_PARTITION" tmpdecrypted
 echo -e "\n"
 
 echo -e "${BBlue}Make filesystem $DISK\n${NC}"
-mkfs --type ext4 --verbose /dev/mapper/tmpdecrypted
+sudo mkfs --type ext4 --verbose /dev/mapper/tmpdecrypted
 echo -e "\n"
 
 sleep 5
 
+echo -e "${BBlue}Set owner $DISK\n${NC}"
+UUID="$(uuidgen)"
+sudo mount --mkdir /dev/mapper/tmpdecrypted /tmp/"$UUID"
+sudo chown "$(id -u)":"$(id -g)" /tmp/"$UUID"
+sudo umount /tmp/"$UUID"
+
 echo -e "${BBlue}Encrypt/close partition \n${NC}"
-cryptsetup --verbose close tmpdecrypted
+sudo cryptsetup --verbose close tmpdecrypted
 echo -e "${BBlue}Done! \n${NC}"
